ba5ec0ba60883f4ef8a2b786150b129da8282c90
Vlad <max2433186@gmail.com>
2021-05-17 10:29:31 +0300

    update reply on cancel message

--- bot.py
+++ bot.py
@@ -231,7 +231,7 @@ def echoForExistUser(update,context):
         'here should be code'
     elif message == 'отмена':
         reply_keyboard = mainMenuKeyboard()
-        update.message.reply_text(None,
+        update.message.reply_text(
                             reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     update.message.reply_text('ты не должен видеть это сообщение')
 
