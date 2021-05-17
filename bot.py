9c1981b9dfc194a5fcee697eb85a630954896e3e
Vlad <max2433186@gmail.com>
2021-05-17 16:14:18 +0300

    debug getDateFromString

--- bot.py
+++ bot.py
@@ -135,12 +135,11 @@ def creatingCardDecription(update,user,message):
 def getDateFromString(update,message):
     try:
         if len(message) < 3:
-            # now = datetime.datetime.now()
-            # dateStr = str(now.year)+'-'+str(now.month)+'-'+message
-            # dateObject = datetime.strptime(dateStr, '%Y-%m-%d').date()
-            date_time_str = '2018-6-29 08:15:27.243860'
-            date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
-            update.message.reply_text(str(date_time_obj),
+            now = datetime.datetime.now()
+            dateStr = str(now.year)+'-'+str(now.month)+'-'+message
+            #date_str = '2018-6-29 08:15:27.243860'
+            dateObject = datetime.datetime.strptime(dateStr, '%Y-%m-%d').date
+            update.message.reply_text(str(dateObject),
                                 reply_markup=ReplyKeyboardRemove())
     except Exception as e:
         update.message.reply_text(str(e))
